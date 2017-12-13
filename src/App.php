0b8d454510809f31dedd03ddfae5e7128370424e
Matt Gill <matthew.gill@digital.cqc.org.uk>
2017-12-13 21:43:15 +0000

    Added ability to update crawler

--- src/App.php
+++ src/App.php
@@ -5,12 +5,8 @@ require_once('app-autoload.php');
 use Cqc\drupal_crawler\Page\BasePage;
 use Cqc\drupal_crawler\Page\TestPage;
 
-$client = new BasePage('https://www.symfony.com/blog/');
+$client = new BasePage('https://github.com/matthew-gill/drupal-scraper');
+//$client = new TestPage('https://www.symfony.com/blog/');
 
 echo $client->getTitle();
-
-
-$client = new TestPage('https://www.symfony.com/blog/');
-
-echo print_r($client->getAllOfTagText('a'));
-
+print_r($client->getAllOfTagText('h4'));
--- src/classes/BasePage.php
+++ src/classes/BasePage.php
@@ -6,40 +6,47 @@ use Goutte\Client;
 use GuzzleHttp\Client as GuzzleClient;
 
 class BasePage {
+	
 	protected $client;
 
 	protected $crawler;
 
-	public function __construct($url) {
-
+	public function __construct($url, $method = 'GET') {
 		$this->client = new Client();
 		$this->client->setClient(new GuzzleClient(array(
 		    'timeout' => 60,
 		    'verify' => false
 		)));
 
-		$this->crawler = $this->client->request('GET', $url);
+		$this->crawler = $this->client->request($method, $url);
 	}
 
-	public function getTitle($default = NULL){
-		$title = trim($this->crawler->filter('title')->first()->text());
+	public function getTitle($default = NULL, $crawler = NULL){
+		$crawler = $this->getCrawler($crawler);
+		$title = trim($crawler->filter('title')->first()->text());
 		if(empty($title)){
 			return $default;
 		}
 		return $title;
 	}
 
-	public function getAllOfTagText($tag){
+	public function getAllOfTagText($tag, $crawler = NULL){
 		$text = [];
-
-		$this->crawler->filter($tag)->each(function ($node, $i) use (&$text) {
+		$crawler = $this->getCrawler($crawler);
+		$crawler->filter($tag)->each(function ($node, $i) use (&$text) {
 		    $selected_text = trim($node->text());
 		    if(!empty($selected_text)){
 		    	$text[] = $selected_text;
 		    }
 		    
 		});
-
 		return $text;
+	}
+
+	private function getCrawler($crawler = NULL) {
+		if($crawler == NULL) {
+			return $this->crawler;
+		}
+		return $crawler;
 	}
 }
