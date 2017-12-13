8d4d25a341b5bca077159c23b01856a2e19a3a00
Matt Gill <matthew.gill@digital.cqc.org.uk>
2017-12-13 20:44:36 +0000

    Got basic link crawl working

--- src/App.php
+++ src/App.php
@@ -1,6 +1,27 @@
 <?php
 require_once('vendor/autoload.php');
 
-use Technofovea\DockerPhpUnitDemo\Calc;
+use Goutte\Client;
+use GuzzleHttp\Client as GuzzleClient;
 
-$calc = new Calc();
+$client = new Client();
+
+
+$client->setClient(new GuzzleClient(array(
+    'timeout' => 60,
+    'verify' => false
+)));
+
+
+
+// Go to the symfony.com website
+$crawler = $client->request('GET', 'https://www.symfony.com/blog/');
+
+// Click on the "Security Advisories" link
+$link = $crawler->selectLink('Security Advisories')->link();
+$crawler = $client->click($link);
+
+// Get the latest post in this category and display the titles
+$crawler->filter('h2 > a')->each(function ($node) {
+    print trim($node->text())."\n";
+});
